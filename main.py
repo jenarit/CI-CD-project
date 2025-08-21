7c9ae687f4fb4bc13cb83024eb5d2e33cd50a813
jenarit <ritimbarajena94@gmail.com>
2025-08-21 15:35:20 +0530

    Your commit message here

--- main.py
+++ main.py
@@ -6,7 +6,3 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 def add(a, b):
     logging.info(f"Adding {a} and {b}")
     return a + b
-
-if __name__ == "__main__":
-    result = add(5, 7)
-    print("Result is:", result)
